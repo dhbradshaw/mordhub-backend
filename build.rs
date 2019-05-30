fadaf2bab0424a48f6ab0192e8ba17e92c011e73
Matt Taylor <mstaveleytaylor@gmail.com>
2019-05-30 23:33:39 +0100

    Update markdown build script for windows

--- build.rs
+++ build.rs
@@ -5,17 +5,24 @@ use std::{
     path::Path,
 };
 
+// Compile markdown files in the markdown folder into tera templates in the templates folder
+
 fn main() -> io::Result<()> {
     let mut options = Options::empty();
     options.insert(Options::ENABLE_STRIKETHROUGH);
 
-    let guide_in_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("markdown/guides");
+    let guide_in_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("markdown/guides/");
     let guide_out_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("templates/guides/gen");
 
-    for guide in fs::read_dir("markdown/guides")? {
+    println!("cargo:rerun-if-changed={}", guide_in_dir.to_str().unwrap());
+
+    let _ = fs::create_dir(&guide_out_dir); // Create if not already exists
+
+    for guide in fs::read_dir(&guide_in_dir)? {
         let guide = guide?;
 
         let path = guide.path();
+        println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
 
         let mut src = fs::File::open(&path)?;
         let mut src_data = String::new();
@@ -30,10 +37,10 @@ fn main() -> io::Result<()> {
         let out_path = guide_out_dir.join(out_file_name.file_name().unwrap());
 
         let mut out = fs::File::create(out_path)?;
-        out.write_all("{% extends \"index.html\" %}\n\n".as_bytes())?;
+        out.write_all("{% extends \"index.html\" %}\n\n{% block content %}\n".as_bytes())?;
         out.write_all(html.as_bytes())?;
+        out.write_all("{% endblock %}\n".as_bytes())?;
     }
 
-    println!("cargo:rerun-if-changed={}", guide_in_dir.to_str().unwrap());
     Ok(())
 }
--- markdown/guides/test.md
+++ markdown/guides/test.md
@@ -1,1 +1,1 @@
-Hello world! ~~foo~~ bar
+Hello world! ~~foo~~ bar **baz**
